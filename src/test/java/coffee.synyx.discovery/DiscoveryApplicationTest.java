a6b05f74cf792b253a9aaf558c55c595d5065b81
Tobias Schneider <tobias.schneider@tobsch.org>
2017-02-24 21:18:03 +0100

    Add context loads test

---
+++ src/test/java/coffee.synyx.discovery/DiscoveryApplicationTest.java
@@ -0,0 +1,22 @@
+package coffee.synyx.discovery;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @author  Tobias Schneider
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
